e230cd9a40c92f76cfdd81b1ec0358da4ecf409e
diptiequals <diptiequals@gmail.com>
2023-03-11 23:27:05 +0530

    11.3.2023

---
+++ JenkinsProject/src/test/java/jenki/JenkiSample.java
@@ -0,0 +1,35 @@
+package jenki;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class JenkiSample {
+	@Test
+	public void myMethod()
+	{
+
+	WebDriver driver=null;
+	String BROWSER=System.getProperty("browser");
+	String URL=System.getProperty("url");
+	System.out.println(BROWSER);
+	if(BROWSER.equalsIgnoreCase("browser"))
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+	}
+	else if(BROWSER.equalsIgnoreCase("firefox"))
+	{
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
+		
+	}
+	
+	driver.get(URL);	
+}
+
+}
